9efe4db02bf4778fdfdf9309fea2d3e3a11d2c5a
Hansheng Zhang <zhang.naville@me.com>
2016-11-15 08:24:26 +0000

    Add More dump() info to DyldCommand

--- src/LCDylibCommand.cpp
+++ src/LCDylibCommand.cpp
@@ -24,6 +24,10 @@ LCDylibCommand::LCDylibCommand(char* LCPosition,bool shouldSwap):NyachOLoadComma
 std::string LCDylibCommand::dump(){
     stringstream ss;
     ss<<NyachOLoadCommand::dump();
-    ss<<"Path: "<<this->pathName;
+    ss<<"Path: "<<this->pathName<<std::endl;
+    time_t time=this->timestamp;
+    ss<<"Timestamp: "<<ctime(&time)<<std::endl;
+    ss<<"Compatibility Version: "<<this->compatibility_version<<std::endl;
+    ss<<"Current Version: "<<this->current_version<<std::endl;
     return ss.str();
 }
